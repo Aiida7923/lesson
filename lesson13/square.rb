be077d6a89ad9130469dbcbd41b5669885ea8a23
Satoshi Odashima <odashimasatoshi@odajimasatoshinoMacBook-puro.local>
2018-12-31 22:31:27 +0900

    edited square.rb

--- lesson13/square.rb
+++ lesson13/square.rb
@@ -2,14 +2,13 @@ class Square < Quadrilateral
 
   attr_reader :side_length
 
-<<<<<<< HEAD
   def initialize(colore,height)
     super(colore,height,height)
-=======
+  end
+  
   def initialize(colore,side_length)
     super(colore,side_length,side_length)
     @side_length = side_length
->>>>>>> origin/master
   end
 
   def info
